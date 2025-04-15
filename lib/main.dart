0d875f249c1d259d7e2dcab3b89c1c439e038a15
Célio Ferreira <celioferreira.dev@gmail.com>
2025-04-15 15:45:24 -0300

    creat provider and hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:shared_preferences_dio/my_app.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main(List<String> args) {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentsDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentsDirectory.path);
   runApp(TrilhaMyApp());
 }
